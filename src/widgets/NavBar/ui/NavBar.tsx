c2a6ad7a51fdb6da1705e62bc38ca4581d7e0c71
Iuliia <tretiakova.iuliia@gmail.com>
2023-03-02 15:47:04 +0300

    fix navbar linting

--- src/widgets/NavBar/ui/NavBar.tsx
+++ src/widgets/NavBar/ui/NavBar.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { classNames } from 'shared/lib/classNames/classNames';
 import { AppLink } from 'shared/ui/AppLink/AppLink';
+import { useTranslation } from 'react-i18next';
 import cls from './NavBar.module.scss';
 
 interface NavBarProps {
@@ -9,11 +10,16 @@ interface NavBarProps {
 
 export const NavBar = (props: NavBarProps) => {
     const { className } = props;
+    const { t } = useTranslation();
     return (
         <div className={classNames(cls.Navbar, {}, [className])}>
             <div className={cls.navLinks}>
-                <AppLink to="/" className={cls.navLinksItem}>Home</AppLink>
-                <AppLink to="/about">About</AppLink>
+                <AppLink to="/" className={cls.navLinksItem}>
+                    {t('Главная')}
+                </AppLink>
+                <AppLink to="/about">
+                    {t('О нас')}
+                </AppLink>
             </div>
         </div>
 
