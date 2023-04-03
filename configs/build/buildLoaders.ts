f78717ab966faf6cdc336b4e6de1a49d68d38431
Iuliia <tretiakova.iuliia@gmail.com>
2023-04-03 15:59:12 +0300

    add babel loader

--- configs/build/buildLoaders.ts
+++ configs/build/buildLoaders.ts
@@ -2,8 +2,11 @@ import webpack from 'webpack';
 import { BuildOptions } from './types/config';
 import { buildCssLoader } from './loaders/buildCssLoader';
 import { buildSvgLoader } from './loaders/buildSvgLoader';
+import { buildBabelLoader } from './loaders/buildBabelLoader';
 
-export const buildLoaders = ({ isDev }: BuildOptions): webpack.RuleSetRule[] => {
+export const buildLoaders = (options: BuildOptions): webpack.RuleSetRule[] => {
+    const { isDev } = options;
+
     const tsLoader = {
         test: /\.tsx?$/,
         use: 'ts-loader',
@@ -32,22 +35,7 @@ export const buildLoaders = ({ isDev }: BuildOptions): webpack.RuleSetRule[] => 
         },
     };
 
-    const babelLoader = {
-        test: /\.(js|jsx|tsx)$/,
-        exclude: /node_modules/,
-        use: {
-            loader: 'babel-loader',
-            options: {
-                presets: ['@babel/preset-env'],
-                plugins: [
-                    ['i18next-extract', {
-                        locales: ['ru', 'en'],
-                        keyAsDefaultValue: true,
-                    }],
-                ],
-            },
-        },
-    };
+    const babelLoader = buildBabelLoader(options);
 
     return [
         fontsLoader,
